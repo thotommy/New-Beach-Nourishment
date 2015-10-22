da324319136aa32ce4897fa5e57e7bf30b259686
Paul Raiche <pdraiche1@agora.cs.wcu.edu>
2015-10-22 17:50:17 -0400

    Added OneState -- Testing it now

--- beach.js
+++ beach.js
@@ -28,7 +28,7 @@ app.get('/oneState', function(req,res) {
     var data = generateOneStateData(state); //this is equivalent of results.php
     //version with all data to be displayed for the state returned in the object
     //assisnged to the variable data
-    res.render('oneState', data); 
+    res.render('oneState', { data:data }); 
         //The first arugument is the name of the template file in the views
         //directory that will be the page displaying the information about the
         //state; that is, the file
@@ -54,4 +54,44 @@ app.listen(app.get('port'), function() {
     console.log('Server starts on http://localhost:' +
                 app.get('port') + '; press Ctrl-C to terminate.');
 });
+
+function generateOneStateData(current_state) {
+	var fullStateName = "";
+	if (current_state == "AL") {
+        fullStateName = "Alabama";
+    } else if (current_state == "CA") {
+        fullStateName = "California";
+    } else if (current_state == "CN") {
+        fullStateName = "Connecticut";
+    } else if (current_state == "DE") {
+        fullStateName = "Delaware";
+    } else if (current_state == "GA") {
+        fullStateName = "Georgia";
+    } else if (current_state == "LA") {
+        fullStateName = "Louisiana";
+    } else if (current_state == "MA") {
+        fullStateName = "Massachusetts";
+    } else if (current_state == "MD") {
+        fullStateName = "Maryland";
+    } else if (current_state == "ME") {
+        fullStateName = "Maine";
+    } else if (current_state == "MS") {
+        fullStateName = "Mississipi";
+    } else if (current_state == "NC") {
+        fullStateName = "North Carolina";
+    } else if (current_state == "NJ") {
+        fullStateName = "New Jersey";
+    } else if (current_state == "RI") {
+        fullStateName = "Rhode Island";
+    } else if (current_state == "SC") {
+        fullStateName = "South Carolina";
+    } else if (current_state == "TX") {
+        fullStateName = "Texas";
+    } else if (current_state == "VA") {
+        fullStateName = "Virginia";
+    } else if (current_state == "WA") {
+        fullStateName = "Washington";
+    }
+	return fullStateName;
+};
 
